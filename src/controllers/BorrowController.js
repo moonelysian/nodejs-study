8aa39242395249f2dbdd981a9ebc8d0e0b689da8
moonelysian <dandyblack@naver.com>
2019-10-13 23:01:28 +0900

    Borrow Controller

--- src/controllers/BorrowController.js
+++ src/controllers/BorrowController.js
@@ -1,1 +1,63 @@
-const BookService = require('../services/BookService');
+const BorrowService = require('../services/BookService');
+const moment = require('moment');
+
+const createBorrow = function(req, res){
+    const borrowService = new BorrowService();
+    const body = req.body;
+    const borrow = borrowService.create(body.userId, body.bookId);
+
+    res.send({
+        data: borrow.toJson(),
+    });
+}
+
+const getBorrowList = function(req, res){
+    const borrowService = new BorrowService();
+    const borrows = borrowService.find();
+
+    res.send({
+        data: borrows,
+    });
+}
+
+const getBorrow = function(){
+    
+}
+
+const getListByUserID = function(){
+
+}
+
+const getListByBookID = function(){
+
+}
+
+const updateDue = function(req, res){
+    const borrowService = new BorrowService();
+    const data = {
+        end: moment().add(7 , 'days').format('YYYY-MM-DD') 
+    }
+    const borrow = borrowService.update(req.params.borrow, data);
+
+    res.send({
+        data: borrow,
+    });
+}
+
+const deleteBorrow = function(req, res){
+    const borrowService = new BorrowService();
+    borrowService.delete(req.params.borrow);
+    res.send({
+        data: 'success'
+    });
+}
+
+module.exports = {
+    createBorrow,
+    getBorrow,
+    getBorrowList,
+    getListByBookID,
+    getListByUserID,
+    updateDue,
+    deleteBorrow,
+}
--- src/services/BorrowService.js
+++ src/services/BorrowService.js
@@ -1,48 +1,54 @@
 const BorrowRepository = require('../database/repositories/BorrowRepository');
 const Borrow = require('../models/Borrow');
 
-const moment = require('moment');
+
 
 class BorrowService{
     constructor(){
         this.borrowRepository = new BorrowRepository();
     }
 
+    // 목록 생성
     create(userId, bookId){
         const borrow = Borrow.newInstance(userId, bookId);
         this.borrowRepository.create(borrow.toJson());
         return borrow;
     }
 
+    //전체 목록
     find(){
         const borrowList = this.borrowRepository.find();
         return borrowList.map(borrowList => Borrow.formData(borrowList));
     }
 
+    //borrow id로 찾기
     findById(id){
         const borrow = Borrow.formData(this.borrowRepository.findById(id));
         return borrow;
     }
 
+    // 특정 사용자가 빌린 책 목록
     findByUserId(userId){
         const borrowList = Borrow.formData(this.borrowRepository.findByUserId(userId));
         return borrowList;
     }
 
+    // 특정 책을 빌린 사용자 목록
     findByBookId(bookId){
         const borrowList = Borrow.formData(this.borrowRepository.findBookId(bookId));
         return borrowList;
     }
 
-    update(id){
+    // 대여기간 연장
+    update(id, data){
         const borrow = Borrow.formData(this.borrowRepository.findById(id));
-
-        borrow.start = moment().format('YYYY-MM-DD');
-        borrow.end = moment().add(7 , 'days').format('YYYY-MM-DD');
+        
+        borrow.end = data.end || borrow.end;
 
         this.borrowRepository.update(id, borrow.toJson());
     }
 
+    // 책 반납
     delete(id){
         this.borrowRepository.delete(id);
     }
